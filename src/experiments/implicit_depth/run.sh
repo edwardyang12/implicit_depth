0304747f35b68cfc5ec9e385551697f57a8362a6
edwardyang12 <edwardyang016@gmail.com>
2022-02-11 23:41:31 -0800

    try run

--- src/experiments/implicit_depth/run.sh
+++ src/experiments/implicit_depth/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 default_cfg_path=./experiments/implicit_depth/default_config.yaml
-cfg_paths=./experiments/implicit_depth/test_refine.yaml
+cfg_paths=./experiments/implicit_depth/train_lidf.yaml
 
 python main.py \
     --default_cfg_path $default_cfg_path \
